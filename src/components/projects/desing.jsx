2ac6f73ac5ba43fccd34173babb14778ea900aa4
Matías Jordán <matiasowjordan@gmail.com>
2025-01-03 07:41:34 -0300

    mejoras visuales v.3

--- src/components/projects/desing.jsx
+++ src/components/projects/desing.jsx
@@ -14,10 +14,10 @@ function MyDesigns() {
             Letterboxd redesign
           </h5>
           <p className="font-normal text-gray-300">
-            Proyecto realizado como proyecto integrador del curso de diseño UX/UI avanzado de Coderhouse.
+            Proyecto realizado en el curso de diseño UX/UI avanzado de Coderhouse en donde se aplican diversos métodos de research, discovery y prototipado.
           </p>
           <div className="flex flex-wrap">
-            <span className="bg-[#28282834] badge gap-1 text-[#fafafa] text-[10px] font-medium inline-flex items-center px-1.5 rounded me-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500 transition-all">
+            <span className="bg-[#28282834] badge gap-1 text-[#fafafa] text-[10px] h-[2em] font-medium inline-flex items-center px-1.5 rounded me-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500 transition-all">
               <svg width="12" height="12" viewBox="0 0 54 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clipPath="url(#clip0_912_3)">
                   <path d="M13.3333 80.0002C20.6933 80.0002 26.6667 74.0268 26.6667 66.6668V53.3335H13.3333C5.97333 53.3335 0 59.3068 0 66.6668C0 74.0268 5.97333 80.0002 13.3333 80.0002Z" fill="#0ACF83" />
@@ -74,6 +74,52 @@ function MyDesigns() {
               </svg>
               Ver proyecto
             </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-sm bg-[#28282830] animate-pulse border border-gray-700 rounded-lg shadow dark:bg-gray-800">
+        <div className="h-[185px]">
+          <img
+            className="rounded-t-lg h-full w-full object-cover object-top"
+            src="/public/cuenta_dni.webp"
+            alt="Cuenta DNI redesign"
+          />
+        </div>
+        <div className="p-5 flex flex-col gap-3">
+          <h5 className="text-xl font-bold tracking-tight text-[#fafafa]">
+            Cuenta DNI redesign
+          </h5>
+          <p className="font-normal text-gray-300">
+            Nuevo proyecto en progreso de ser realizado. Se utiliza la metodología de OOUX - Diseño de experiencia de usuario orientado a objetos. 
+          </p>
+          <div className="flex flex-wrap">
+            <span className="bg-[#28282834] badge gap-1 text-[#fafafa] text-[10px] h-[2em] font-medium inline-flex items-center px-1.5 rounded me-2 dark:bg-gray-700 dark:text-gray-400 border border-gray-500 transition-all">
+              <svg width="12" height="12" viewBox="0 0 54 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_912_3)">
+                  <path d="M13.3333 80.0002C20.6933 80.0002 26.6667 74.0268 26.6667 66.6668V53.3335H13.3333C5.97333 53.3335 0 59.3068 0 66.6668C0 74.0268 5.97333 80.0002 13.3333 80.0002Z" fill="#0ACF83" />
+                  <path d="M0 39.9998C0 32.6398 5.97333 26.6665 13.3333 26.6665H26.6667V53.3332H13.3333C5.97333 53.3332 0 47.3598 0 39.9998Z" fill="#A259FF" />
+                  <path d="M0 13.3333C0 5.97333 5.97333 0 13.3333 0H26.6667V26.6667H13.3333C5.97333 26.6667 0 20.6933 0 13.3333Z" fill="#F24E1E" />
+                  <path d="M26.6667 0H40.0001C47.3601 0 53.3334 5.97333 53.3334 13.3333C53.3334 20.6933 47.3601 26.6667 40.0001 26.6667H26.6667V0Z" fill="#FF7262" />
+                  <path d="M53.3334 39.9998C53.3334 47.3598 47.3601 53.3332 40.0001 53.3332C32.6401 53.3332 26.6667 47.3598 26.6667 39.9998C26.6667 32.6398 32.6401 26.6665 40.0001 26.6665C47.3601 26.6665 53.3334 32.6398 53.3334 39.9998Z" fill="#1ABCFE" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_912_3">
+                    <rect width="53.3333" height="80" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              Figma
+            </span>
+          </div>
+          <div className="flex justify-between w-full gap-4">
+            
+            <span
+              className="w-full border cursor-not-allowed disabled gap-2 border-gray-700 bg-gray-600 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-500 rounded-lg hover:bg-gray-600"
+            >
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-tools"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21h4l13 -13a1.5 1.5 0 0 0 -4 -4l-13 13v4" /><path d="M14.5 5.5l4 4" /><path d="M12 8l-5 -5l-4 4l5 5" /><path d="M7 8l-1.5 1.5" /><path d="M16 12l5 5l-4 4l-5 -5" /><path d="M16 17l-1.5 1.5" /></svg>
+              En trabajo...
+            </span>
           </div>
         </div>
       </div>
