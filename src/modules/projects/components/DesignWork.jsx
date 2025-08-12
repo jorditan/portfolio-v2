5c1151696f8068087c88c47e8c09518c09dd9a36
Matías Jordán <matiasowjordan@gmail.com>
2025-08-11 21:46:54 -0300

    añado proyecto UX/UI

--- src/modules/projects/components/DesignWork.jsx
+++ src/modules/projects/components/DesignWork.jsx
@@ -59,11 +59,11 @@ function DesignWork() {
               Figma
             </span>
           </div>
-          <div className="flex justify-between w-full gap-4">
+          <div className="flex justify-end w-full gap-4">
             <a
               href="https://www.behance.net/gallery/208245225/Letterboxd-redesign-Case-of-study-UXUI"
               target="_blank"
-              className="w-full border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
+              className="w-fit border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
             >
               <svg
                 height="22px"
@@ -162,7 +162,106 @@ function DesignWork() {
             <a
               href="https://test-phishing.hackmetrix.com/"
               target="_blank"
-              className="w-full border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
+              className="w-fit border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
+            >
+              <svg
+                height="22px"
+                width="22px"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#fafafa"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  stroke="#fafafa"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <rect x="0" fill="none" width="12" height="12"></rect>{" "}
+                  <g>
+                    {" "}
+                    <path d="M18.3 9.5C15 4.9 8.5 3.8 3.9 7.2c-1.2.9-2.2 2.1-3 3.4.2.4.5.8.8 1.2 3.3 4.6 9.6 5.6 14.2 2.4.9-.7 1.7-1.4 2.4-2.4.3-.4.5-.8.8-1.2-.3-.4-.5-.8-.8-1.1zm-8.2-2.3c.5-.5 1.3-.5 1.8 0s.5 1.3 0 1.8-1.3.5-1.8 0-.5-1.3 0-1.8zm-.1 7.7c-3.1 0-6-1.6-7.7-4.2C3.5 9 5.1 7.8 7 7.2c-.7.8-1 1.7-1 2.7 0 2.2 1.7 4.1 4 4.1 2.2 0 4.1-1.7 4.1-4v-.1c0-1-.4-2-1.1-2.7 1.9.6 3.5 1.8 4.7 3.5-1.7 2.6-4.6 4.2-7.7 4.2z"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+              Ver proyecto
+            </a>
+          </div>
+        </div>
+      </div>
+
+        <div className="max-w-sm dark:border bg-gray-100 border-2 border-gray-700 rounded-lg shadow dark:bg-gray-800">
+        <div className="h-[185px]">
+          <img
+            className="rounded-t-lg h-full w-full object-cover"
+            src="/redesign_legales.webp"
+            alt="Test phishing Hackmetrix"
+          />
+        </div>
+        <div className="p-5 flex flex-col gap-3">
+          <h5 className="text-xl font-bold tracking-tight text-[#141a28] dark:text-[#fafafa]">
+            Diseño módulo de legales
+          </h5>
+          <p className="font-normal dark:text-gray-300 text-gray-800">
+            Proyecto realizado para{" "}
+            <a
+              target="_blank"
+              className="underiline text-sky-500"
+              href="www.hackmetrix.com"
+            >
+              Hackmetrix
+            </a>
+            . Su objetivo fue sustituir procesos manuales en Excel para gestionar
+            requisitos legales, reduciendo tiempo y costos.
+          </p>
+          <div className="flex flex-wrap">
+            <span className="dark:bg-[#28282834] bg-gray-100 badge gap-1 py-1 dark:text-[#fafafa] text-gray-800 text-[10px] font-medium inline-flex items-center px-1.5 rounded me-2 dark:bg-gray-700 border border-gray-500 transition-all">
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 54 80"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_912_3)">
+                  <path
+                    d="M13.3333 80.0002C20.6933 80.0002 26.6667 74.0268 26.6667 66.6668V53.3335H13.3333C5.97333 53.3335 0 59.3068 0 66.6668C0 74.0268 5.97333 80.0002 13.3333 80.0002Z"
+                    fill="#0ACF83"
+                  />
+                  <path
+                    d="M0 39.9998C0 32.6398 5.97333 26.6665 13.3333 26.6665H26.6667V53.3332H13.3333C5.97333 53.3332 0 47.3598 0 39.9998Z"
+                    fill="#A259FF"
+                  />
+                  <path
+                    d="M0 13.3333C0 5.97333 5.97333 0 13.3333 0H26.6667V26.6667H13.3333C5.97333 26.6667 0 20.6933 0 13.3333Z"
+                    fill="#F24E1E"
+                  />
+                  <path
+                    d="M26.6667 0H40.0001C47.3601 0 53.3334 5.97333 53.3334 13.3333C53.3334 20.6933 47.3601 26.6667 40.0001 26.6667H26.6667V0Z"
+                    fill="#FF7262"
+                  />
+                  <path
+                    d="M53.3334 39.9998C53.3334 47.3598 47.3601 53.3332 40.0001 53.3332C32.6401 53.3332 26.6667 47.3598 26.6667 39.9998C26.6667 32.6398 32.6401 26.6665 40.0001 26.6665C47.3601 26.6665 53.3334 32.6398 53.3334 39.9998Z"
+                    fill="#1ABCFE"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_912_3">
+                    <rect width="53.3333" height="80" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              Figma
+            </span>
+          </div>
+          <div className="flex w-full justify-end gap-4">
+            <a
+              href="https://test-phishing.hackmetrix.com/"
+              target="_blank"
+              className="w-fit border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
             >
               <svg
                 height="22px"
