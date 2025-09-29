477e1767d463eb580b44a9023d6d81be6a6238c8
Matías Jordán <matiasowjordan@gmail.com>
2025-09-29 20:49:14 -0300

    features/nueva-ui-proyectos

--- src/common/components/reactComponentes/ThemeSelector.jsx
+++ src/common/components/reactComponentes/ThemeSelector.jsx
@@ -24,14 +24,7 @@ function ThemeSelector() {
 
   return (
     <>
-      <div
-        id="themeButton"
-        role="tooltip"
-        className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-[#fafafa] dark:text-gray-900 bg-[#0f172a] dark:bg-white border dark:border-gray-200 rounded-lg shadow-sm opacity-0 tooltip"
-      >
-        Cambiar tema
-        <div className="tooltip-arrow" data-popper-arrow></div>
-      </div>
+
       <button
         onClick={handleToggle}
         data-tooltip-target="themeButton"
@@ -43,6 +36,14 @@ function ThemeSelector() {
           <MoonIcon className={`transition-all hover:scale-110 ${classIcon}`} />
         )}
       </button>
+      <div
+        id="themeButton"
+        role="tooltip"
+        className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-[#fafafa] dark:text-gray-900 bg-[#0f172a] dark:bg-white border dark:border-gray-200 rounded-lg shadow-sm opacity-0 tooltip"
+      >
+        Cambiar tema
+        <div className="tooltip-arrow" data-popper-arrow></div>
+      </div>
     </>
   );
 }
--- src/modules/projects/components/CardProject.jsx
+++ src/modules/projects/components/CardProject.jsx
@@ -12,92 +12,94 @@ function CardProject({
   showGif,
 }) {
   return (
-    <div className="max-w-sm dark:border bg-gray-100 border-2 border-gray-700 rounded-lg shadow dark:bg-gray-800">
-      <div className="h-[185px]">
-        <img
-          className="rounded-t-lg h-full w-full object-cover"
-          src={image}
-          alt={altImage}
-        />
-      </div>
-      <div className="p-5 flex flex-col gap-3">
-        <h5 className="text-xl font-bold tracking-tight text-[#141a28] dark:text-[#fafafa]">
-          {title}
-        </h5>
-        <p className="font-normal dark:text-gray-300 text-gray-800">
-          {content}
-        </p>
-
-        <div className="flex flex-wrap">
-          {tags.map((tag, i) => (
-            <div
-              key={i}
-              className="dark:bg-[#28282834] bg-gray-200 badge gap-1 dark:text-[#fafafa] text-gray-800 text-[10px] font-medium inline-flex items-center px-1.5 rounded me-2 py-1 dark:bg-gray-700 border border-gray-500 transition-all"
-            >
-              <span className="mr-1">{tag.icon}</span>
-              <span className="text-[12px]">{tag.text}</span>
-            </div>
-          ))}
+    <div className="p-3 border-solid bg-gray-200 hadow-sm bg-transparent backdrop-blur-md hover:rotate-2 transition-all rounded-lg border-2 isolate aspect-video dark:bg-white/10 ring-1 ring-black/ dark:border-gray-500 hover:dark:border-gray-300 border-gray-500 hover:border-gray-900 hover:backdrop-blur-lg">
+      <div className="max-w-sm dark:border bg-gray-200 border-2 border-gray-700 rounded-md shadow-md  dark:bg-gray-800">
+        <div className="h-[185px]">
+          <img
+            className="rounded-t-lg h-full w-full object-cover"
+            src={image}
+            alt={altImage}
+          />
         </div>
+        <div className="p-5 flex flex-col gap-3">
+          <h5 className="text-xl font-bold tracking-tight text-[#141a28] dark:text-[#fafafa]">
+            {title}
+          </h5>
+          <p className="font-normal dark:text-gray-300 text-gray-800">
+            {content}
+          </p>
 
-        <div className="flex justify-between w-full gap-4" id="buttons">
-          <a
-            href={links[0].url}
-            target="_blank"
-            className="w-full border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
-          >
-            <GithubIcon />
-            {links[0].label}
-          </a>
+          <div className="flex flex-wrap">
+            {tags.map((tag, i) => (
+              <div
+                key={i}
+                className="dark:bg-[#28282834] bg-gray-200 badge gap-1 dark:text-[#fafafa] text-gray-800 text-[10px] font-medium inline-flex items-center px-1.5 rounded me-2 py-1 dark:bg-gray-700 border border-gray-500 transition-all"
+              >
+                <span className="mr-1">{tag.icon}</span>
+                <span className="text-[12px]">{tag.text}</span>
+              </div>
+            ))}
+          </div>
 
-          {showGif ? (
-            <Modal
-              buttonText={"Ver GIF"}
-              title={title}
-              text={
-                <>
-                  Este proyecto no puedo ser deployeado porque fue desarrollado
-                  en{" "}
-                  <a
-                    href="https://www.wollok.org/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 underline"
-                  >
-                    Wollok
-                  </a>
-                  , un lenguaje de programación y entorno de desarrollo
-                  integrado para ser utilizado en el dictado de materias
-                  iniciales de programación.
-                  <br />
-                  <br />
-                  Por mi parte, fue utilizando en la materia{" "}
-                  <strong>Programación Orientada a Objetos I</strong> de la{" "}
-                  <strong>UNAHUR</strong>.
-                </>
-              }
-              video={
-                <img
-                  src="/gif-wollolok.gif"
-                  alt="GIF del juego Wollolok"
-                  className="rounded-sm"
-                ></img>
-              }
-            />
-          ) : (
+          <div className="flex justify-between w-full gap-4" id="buttons">
             <a
-              aria-label="Ver proyecto"
-              href={links[1].url}
+              href={links[0].url}
               target="_blank"
-              className="w-full border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white rounded-lg hover:border-gray-500 hover:bg-gray-800 dark:hover:bg-gray-950"
+              className="w-full border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white hover:border-gray-500 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950"
             >
-              <ProjectIcon />
-              {links[1].label}
+              <GithubIcon />
+              {links[0].label}
             </a>
-          )}
+
+            {showGif ? (
+              <Modal
+                buttonText={"Ver GIF"}
+                title={title}
+                text={
+                  <>
+                    Este proyecto no puedo ser deployeado porque fue desarrollado
+                    en{" "}
+                    <a
+                      href="https://www.wollok.org/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 underline"
+                    >
+                      Wollok
+                    </a>
+                    , un lenguaje de programación y entorno de desarrollo
+                    integrado para ser utilizado en el dictado de materias
+                    iniciales de programación.
+                    <br />
+                    <br />
+                    Por mi parte, fue utilizando en la materia{" "}
+                    <strong>Programación Orientada a Objetos I</strong> de la{" "}
+                    <strong>UNAHUR</strong>.
+                  </>
+                }
+                video={
+                  <img
+                    src="/gif-wollolok.gif"
+                    alt="GIF del juego Wollolok"
+                    className="rounded-sm"
+                  ></img>
+                }
+              />
+            ) : (
+              <a
+                aria-label="Ver proyecto"
+                href={links[1].url}
+                target="_blank"
+                className="w-full border gap-2 text-[12px] sm:text-sm border-gray-700 bg-gray-700 dark:bg-gray-900 inline-flex items-center px-3 py-2 font-medium text-center text-white rounded-lg hover:border-gray-500 hover:bg-gray-800 dark:hover:bg-gray-950"
+              >
+                <ProjectIcon />
+                {links[1].label}
+              </a>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
